b49a6fd179177e27b465470a73bf98f4290b24a4
varun-2024 <v.talwar2024@gmail.com>
2025-07-24 19:54:19 +0100

    Area of Square

---
+++ Java DSA/code07.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class code07 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the lenght of side of the square : ");
+        float side = sc.nextFloat();
+        float area = side * side;
+        System.out.println("Area of the Square = " + area);
+    }
+    
+}
