5de24119f0ebb34b77bb53dbf0b01c5814b9eafa
varun-2024 <v.talwar2024@gmail.com>
2025-07-24 12:10:03 +0100

    Data Type sizes

---
+++ Java DSA/code02.java
@@ -0,0 +1,16 @@
+public class code02 {
+    public static void main(String args[]){
+        byte b = 8; // 1 byte -128 to 127 Byte Type store upto 256 numbers 
+        System.out.println(b);
+        char a = 'a'; // 2 bytes Single Characters
+        System.out.println(a);
+        boolean v = true; // 1 byte 0 or 1 True or False in small
+        float price = 10.1f; // Have to add f at the end for numeric literals, treated as double not float
+        float value = (float) 10.5; //4 bytes Or define like this for literals
+        int number = 25; // 4 bytes
+        long pen = 10; // 8 bytes for big intigers
+        double pi = 3.1345; // 8 bytes Big decimal values use double
+        short num  = 240; // 2 bytes -32768 to 32767
+    }
+    
+}
