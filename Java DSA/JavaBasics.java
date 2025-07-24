a1b27df35016f94a149d675a4a9132440dd83a82
varun-2024 <v.talwar2024@gmail.com>
2025-07-24 11:43:58 +0100

    Java

---
+++ Java DSA/JavaBasics.java
@@ -0,0 +1,7 @@
+public class JavaBasics {
+    public static void main(String args[]){
+        System.out.print("Hello World!!!"); // does not give space prints in same line
+        System.out.println("Hello World!!!"); // gives space next thing prints in next line
+        System.out.println("\nHello World!!!"); // \n for next line
+    }
+}
