b0952737b7c4c65493f8b7cada41a738811757f3
varun-2024 <v.talwar2024@gmail.com>
2025-07-25 15:58:25 +0100

    Matrix Input and Output

--- Java DSA/code10InbuiltSort.java
+++ Java DSA/code10InbuiltSort.java
@@ -6,12 +6,23 @@ public class code10InbuiltSort {
         }
         System.out.println();
     }
+    public static void printArr(Integer arr[]){
+        for(int i =0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
     public static void main(String args[]){
         int arr[] = {1, 5, 7, 9 , 0, 2, -1, -3};
         int arr2[] = {4, 2, 1, 0, 3, 9, 7};
-        Arrays.sort(arr);
-        Arrays.sort(arr2, 0, 4);
+        Integer arr3[] = {5, 3, 9, -1, 0, 19 };
+        Arrays.sort(arr); // Sorting Ascending Order Only O(n logn)
+        Arrays.sort(arr2, 0, 4); // Sort Ascending Order - from Start index to End Index(End Index Excluded)
         printArr(arr);
         printArr(arr2);
+        Arrays.sort(arr3, 0, 3, Collections.reverseOrder()); // ReverseOrder does not work on Normal Int Arrays. For this we declare Integer Arr[]. int > Integer.
+        // Int is Primitive Arrays DataType Arrays.sort(arr) ascending only
+        // Integer is an Object Array, supports comparator based sorting like Collections.reverseOrder() Descending Order
+        printArr(arr3);
     }
 }
